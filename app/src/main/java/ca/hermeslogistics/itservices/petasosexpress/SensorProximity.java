6dbf94c2ae31e5808611a19bf57f786349629f03
wmargalik <william.margalik@gmail.com>
2023-09-26 18:59:50 -0400

    PetasosExpress 0.2 (Java Comments Commit): Full Name, student ID, and secHon number all commented on SensorBalance.java. SensorDistance.java, SensorMotors.java, SensorProximity.java

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SensorProximity.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SensorProximity.java
@@ -1,3 +1,8 @@
+/*
+ * Names: Illia M. Popov, William Margalik, Dylan Ashton, Ahmad Aljawish
+ * Student ID: n01421791, n01479878, n01442206, n01375348
+ * Section: B
+ */
 package ca.hermeslogistics.itservices.petasosexpress;
 
 import android.os.Bundle;
