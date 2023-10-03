e53f7ca2dec48572f2d8cb09ae1aa041e7674305
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-10-03 19:03:48 -0400

    PetasosExpress 0.24 (Login Commit): Login Screen UI Demo was finished.

---
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/LoginScreen.java
@@ -0,0 +1,14 @@
+package ca.hermeslogistics.itservices.petasosexpress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
