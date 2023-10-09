f3af57a5afbbe2217423ef7af5c0fe7f1c64322f
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-10-09 15:17:31 -0400

    PetasosExpress 0.53 (Login Commit): Now login reaches FireStore DataBase and checks if user has a Petasos Account.

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/LoginScreen.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/LoginScreen.java
@@ -2,7 +2,18 @@ package ca.hermeslogistics.itservices.petasosexpress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 /*
  * Names: Illia M. Popov, William Margalik, Dylan Ashton, Ahmad Aljawish
  * Student ID: n01421791, n01479878, n01442206, n01375348
@@ -10,9 +21,47 @@ import android.os.Bundle;
  */
 public class LoginScreen extends AppCompatActivity {
 
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+
+        // Initializing Firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        // Initializing views
+        emailEditText = findViewById(R.id.editTextEmail);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        loginButton = findViewById(R.id.buttonLogin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                // Authentication of the user using Firebase Authentication
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginScreen.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign-in successful
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent intent = new Intent(LoginScreen.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // Sign-in failed
+                                    Toast.makeText(LoginScreen.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
---
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SignupScreen.java
@@ -0,0 +1,14 @@
+package ca.hermeslogistics.itservices.petasosexpress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_screen);
+    }
+}
