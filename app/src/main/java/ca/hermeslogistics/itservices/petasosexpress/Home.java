d9fcd730b8171c26dab5798d22b7bfd347275a94
wmargalik <william.margalik@gmail.com>
2023-10-09 16:13:18 -0400

    PetasosExpress 0.57 (Revamped and updated the Home screen to include different categories of image buttons depending on the user's order, as well for all other enquires can be used for the search bar)

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/Home.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/Home.java
@@ -1,5 +1,6 @@
 package ca.hermeslogistics.itservices.petasosexpress;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +11,10 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-/*
- * Names: Illia M. Popov, William Margalik, Dylan Ashton, Ahmad Aljawish
- * Student ID: n01421791, n01479878, n01442206, n01375348
- * Section: B
- */
+
 public class Home extends Fragment {
 
     private EditText searchEditText;
-    private Button placeOrderButton;
 
     public Home() {
         // Required empty public constructor
@@ -27,31 +23,29 @@ public class Home extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view;
+
+        int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // Load landscape layout
+            view = inflater.inflate(R.layout.home_landscape, container, false);
+        } else {
+            // Load portrait layout
+            view = inflater.inflate(R.layout.fragment_home, container, false);
+        }
 
         // Initialize UI elements
         searchEditText = view.findViewById(R.id.searchEditText);
-        placeOrderButton = view.findViewById(R.id.placeOrderButton);
 
         // Set click listener for the search button
         searchEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Handle search functionality here
                 String searchQuery = searchEditText.getText().toString();
                 // Perform search based on searchQuery
-
-                Toast.makeText(getActivity(), "Searching for: " + searchQuery, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // Set click listener for the place order button
-        placeOrderButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle place order functionality here
-                // For example, you can open a new activity or fragment to place an order
-                Toast.makeText(getActivity(), "Placing order...", Toast.LENGTH_SHORT).show();
+                String message = getString(R.string.searching_for, searchQuery);
+                Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
             }
         });
 
