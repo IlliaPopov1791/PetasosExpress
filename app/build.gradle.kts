d9fef595406f98fedfc1968c19bae37fb7cfa4fe
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-11-04 17:49:25 -0400

    PetasosExpress 0.79 (Distance Sensor Commit): data from FireStore DataBase is retrieved and showed on the screen. Later sensor data will be sent and stored there in realtime, but for now we'll use manually entered data for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.google.firebase:firebase-auth:22.1.2")
+    implementation ("com.google.firebase:firebase-firestore:24.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SensorDistance.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SensorDistance.java
@@ -1,20 +1,34 @@
 package ca.hermeslogistics.itservices.petasosexpress;
 
-import android.content.res.Configuration;
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 /*
  * Names: Illia M. Popov, William Margalik, Dylan Ashton, Ahmad Aljawish
  * Student ID: n01421791, n01479878, n01442206, n01375348
  * Section: B
  */
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.Locale;
+
+import javax.annotation.Nullable;
+
 public class SensorDistance extends Fragment {
+
+    // TextView for displaying the value
+    private TextView txtValue;
+
+    // Firestore database reference
+    private FirebaseFirestore db;
 
     public SensorDistance() {
         // Required empty public constructor
@@ -22,14 +36,59 @@ public class SensorDistance extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Initialize Firestore
+        db = FirebaseFirestore.getInstance();
+
         // Determine the device's current orientation
         int orientation = getResources().getConfiguration().orientation;
 
+        View view;
         // Load the appropriate layout based on orientation
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            return inflater.inflate(R.layout.sensor_distance, container, false);
+            view = inflater.inflate(R.layout.sensor_distance, container, false);
         } else {
-            return inflater.inflate(R.layout.sensor_distance_landscape, container, false);
+            view = inflater.inflate(R.layout.sensor_distance_landscape, container, false);
         }
+
+        // Get a reference to the TextView
+        txtValue = view.findViewById(R.id.txtValue);
+
+        // Get the document reference from Firestore
+        DocumentReference docRef = db.collection("Distance (Ultrasonic Sensor)").document("HChcHZOZwRMcrFVLnar4");
+
+        // Listen for realtime updates
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    // Handle error - !DO Later!
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    // Retrieve the 'Pulse Duration' and 'Speed of Sound' fields
+                    Double pulseDuration = snapshot.getDouble("Pulse Duration");
+                    Double speedOfSound = snapshot.getDouble("Speed of Sound ");
+
+                    // Check if both fields are retrieved successfully
+                    if (pulseDuration != null && speedOfSound != null) {
+                        // Calculate the distance
+                        Double distance = pulseDuration * speedOfSound;
+
+                        txtValue.setText(String.format(Locale.getDefault(), "%.2f", distance));
+                    } else {
+                        //If the fields are not found or null
+                        txtValue.setText(R.string.no_data);
+                    }
+                } else {
+                    //If document does not exist
+                    txtValue.setText(R.string.no_data);
+                }
+            }
+        });
+
+        return view;
     }
 }
+
