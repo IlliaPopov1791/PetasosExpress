d5a77937a9b261577f7266d0220c8974cfb2e205
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-11-05 16:16:19 -0400

    PetasosExpress 0.88 (GPS Sensor Commit): data from FireStore DataBase is retrieved and used to determine location that is shown on the map. Map follows marker that changes its location based on changes in retrieving data. Later sensor data will be sent and stored there in realtime, but for now we'll use manually entered data for testing.

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SensorGPS.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SensorGPS.java
@@ -1,34 +1,102 @@
 package ca.hermeslogistics.itservices.petasosexpress;
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 /*
  * Names: Illia M. Popov, William Margalik, Dylan Ashton, Ahmad Aljawish
  * Student ID: n01421791, n01479878, n01442206, n01375348
  * Section: B
  */
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+
 public class SensorGPS extends Fragment implements OnMapReadyCallback {
 
+    private static final int MY_LOCATION_REQUEST_CODE = 1;
+    private GoogleMap mMap;
+    private Marker currentLocationMarker;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.sensor_gps, container, false);
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
         return view;
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap){
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
 
+        //Checking if we have location permission
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+        listenForLocationUpdates();
+    }
 
+    private void listenForLocationUpdates() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference locationRef = db.collection("Coordinates").document("43nKOPpvl1ZP7Gimojoo");
+
+        // Listen for real-time updates
+        locationRef.addSnapshotListener((snapshot, e) -> {
+            if (e != null) {
+                // Handle error
+                return;
+            }
+
+            if (snapshot != null && snapshot.exists()) {
+                GeoPoint geoPoint = snapshot.getGeoPoint("Location");
+                if (geoPoint != null) {
+                    LatLng newLocation = new LatLng(geoPoint.getLatitude(), geoPoint.getLongitude());
+                    updateMap(newLocation);
+                }
+            }
+        });
+    }
+
+    private void updateMap(LatLng location) {
+        if (mMap != null) {
+            if (currentLocationMarker != null) {
+                currentLocationMarker.remove();
+            }
+            currentLocationMarker = mMap.addMarker(new MarkerOptions().position(location).title("Current Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+        }
+    }
+
+    private void requestLocationPermission() {
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_LOCATION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
+                }
+            } else {
+                //No marker :(
+            }
+        }
     }
 }
