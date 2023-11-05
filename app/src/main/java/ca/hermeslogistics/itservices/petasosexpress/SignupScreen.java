14a7c186d4a5248e9754a957242cc613b80c49d1
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-11-04 22:01:10 -0400

    PetasosExpress 0.81 (Registration DB Update): Registration now creates a record of Name, Email and Phone number in a FireStore with email that can be used to link a record with a user account in Firebase Authentication.

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SignupScreen.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SignupScreen.java
@@ -3,16 +3,24 @@ package ca.hermeslogistics.itservices.petasosexpress;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * Names: Illia M. Popov, William Margalik, Dylan Ashton, Ahmad Aljawish
  * Student ID: n01421791, n01479878, n01442206, n01375348
@@ -20,12 +28,18 @@ import com.google.firebase.auth.AuthResult;
  */
 public class SignupScreen extends AppCompatActivity {
 
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
     private EditText emailEditText;
+    private EditText phoneEditText;
     private EditText passwordEditText;
+    private EditText confirmPasswordEditText;
     private Button loginButton;
     private Button signupButton;
 
+    //Firebase
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,43 +52,63 @@ public class SignupScreen extends AppCompatActivity {
             setContentView(R.layout.signup_screen_landscape);
         }
 
-        // Initialize Firebase Authentication
+        //Initialize Firebase Authentication and Firestore
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
-        // Initialize views
+        //Initialize views
+        firstNameEditText = findViewById(R.id.editTextFirstName);
+        lastNameEditText = findViewById(R.id.editTextLastName);
         emailEditText = findViewById(R.id.editTextEmail);
+        phoneEditText = findViewById(R.id.editTextPhoneNumber);
         passwordEditText = findViewById(R.id.editTextPassword);
+        confirmPasswordEditText = findViewById(R.id.editTextConfirmPassword);
         signupButton = findViewById(R.id.buttonSignUp);
         loginButton = findViewById(R.id.buttonLogin);
 
-
-        // Implement a click listener for the signup button
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Get user input
-                final String email = emailEditText.getText().toString();
-                final String password = passwordEditText.getText().toString();
+                //Getting user input
+                final String email = emailEditText.getText().toString().trim();
+                final String password = passwordEditText.getText().toString().trim();
+                final String confirmPassword = confirmPasswordEditText.getText().toString().trim();
+                final String firstName = firstNameEditText.getText().toString().trim();
+                final String lastName = lastNameEditText.getText().toString().trim();
+                final String phone = phoneEditText.getText().toString().trim();
 
-                // Create user in Firebase Authentication
+                //Checking if any field is empty
+                if (TextUtils.isEmpty(firstName) || TextUtils.isEmpty(lastName) ||
+                        TextUtils.isEmpty(email) || TextUtils.isEmpty(phone) ||
+                        TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
+                    Toast.makeText(SignupScreen.this, (R.string.all_fields_filled), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Checking if passwords match
+                if (!password.equals(confirmPassword)) {
+                    Toast.makeText(SignupScreen.this, (R.string.passwords_do_not_match), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Creating user in Firebase Authentication
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(SignupScreen.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-                                    // Sign-up successful
-                                    Toast.makeText(SignupScreen.this, R.string.registration_successful, Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(SignupScreen.this, MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
+                                    //Success
+                                    addUserToFirestore(email, firstName, lastName, phone);
                                 } else {
-                                    // Sign-up failed
-                                    Toast.makeText(SignupScreen.this, R.string.registration_failed_please_try_again, Toast.LENGTH_SHORT).show();
+                                    //Fail
+                                    Toast.makeText(SignupScreen.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
                                 }
                             }
                         });
             }
         });
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -82,5 +116,29 @@ public class SignupScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void addUserToFirestore(String email, String firstName, String lastName, String phone) {
+        //Creating a new user record with a first name, last name, email, and phone
+        Map<String, Object> user = new HashMap<>();
+        user.put("firstName", firstName);
+        user.put("lastName", lastName);
+        user.put("email", email);
+        user.put("phone", Long.parseLong(phone)); // Store as a number
+
+        //Adding a new document with the email as the ID to link with Authentication
+        db.collection("userInfo").document(email)
+                .set(user)
+                .addOnSuccessListener(aVoid -> {
+                    //Success
+                    Toast.makeText(SignupScreen.this, (R.string.registration_successful), Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(SignupScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    //Fail
+                    Toast.makeText(SignupScreen.this, (R.string.registration_failed_please_try_again), Toast.LENGTH_SHORT).show();
+                });
     }
 }
