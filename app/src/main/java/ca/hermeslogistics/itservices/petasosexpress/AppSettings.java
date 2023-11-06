55a79e1566231c0049a1f3ac0d2e1d97a01fb7dc
ahmadaljawish5348 <Ahmadjaweesh6@gmail.com>
2023-11-05 23:45:06 -0500

    PetasosExpress 0.90 (settings Commit): adding the functionality to save users input into sharedPref

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/AppSettings.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/AppSettings.java
@@ -1,15 +1,18 @@
+
 package ca.hermeslogistics.itservices.petasosexpress;
 
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.ToggleButton;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
@@ -20,6 +23,12 @@ import androidx.fragment.app.Fragment;
  * Section: B
  */
 public class AppSettings extends Fragment {
+
+    // Constants for SharedPreferences keys
+    private static final String PREFS_NAME = "UserSettings";
+    private static final String THEME_KEY = "theme_mode";
+    private static final String ORIENTATION_KEY = "orientation_mode";
+    private static final String ADDRESS_KEY = "default_address";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,8 +42,35 @@ public class AppSettings extends Fragment {
             view = inflater.inflate(R.layout.app_setttings_landscape, container, false);
         }
 
-
+        // Initialize ToggleButtons and EditText
         ToggleButton toggleTheme = view.findViewById(R.id.toggle_theme);
+        ToggleButton toggleOrientation = view.findViewById(R.id.toggle_portrait_landscape);
+        EditText editTextAddress = view.findViewById(R.id.editTextText);
+
+        // Retrieve saved settings
+        SharedPreferences settings = getActivity().getSharedPreferences(PREFS_NAME, 0);
+        boolean isDarkMode = settings.getBoolean(THEME_KEY, false);
+        boolean isLandscape = settings.getBoolean(ORIENTATION_KEY, false);
+        String savedAddress = settings.getString(ADDRESS_KEY, "Address"); // Default value is "Address"
+
+        // Set saved values
+        toggleTheme.setChecked(isDarkMode);
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+        toggleOrientation.setChecked(isLandscape);
+        if (isLandscape) {
+            getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        } else {
+            getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+
+        editTextAddress.setText(savedAddress);
+
+        // Set onClickListeners for the ToggleButtons
         toggleTheme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,11 +84,14 @@ public class AppSettings extends Fragment {
                     DisplayToast(getString(R.string.light_theme_switched));
                 }
                 getActivity().recreate();
+
+                // Save the theme setting
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean(THEME_KEY, toggleTheme.isChecked());
+                editor.apply();
             }
         });
 
-        // ToggleButton for orientation
-        ToggleButton toggleOrientation = view.findViewById(R.id.toggle_portrait_landscape);
         toggleOrientation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,6 +104,27 @@ public class AppSettings extends Fragment {
                     getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
                     DisplayToast(getString(R.string.portrait_orientation));
                 }
+
+                // Save the orientation setting
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean(ORIENTATION_KEY, toggleOrientation.isChecked());
+                editor.apply();
+            }
+        });
+
+        // Save address when it changes
+        editTextAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString(ADDRESS_KEY, s.toString());
+                editor.apply();
             }
         });
 
