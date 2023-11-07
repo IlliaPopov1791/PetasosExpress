f2cb15ae733b0a2cda40be09ea17b04293544d06
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-11-07 18:39:28 -0400

    PetasosExpress 0.96 (Feedback Functionality Implemented): feedback now is sent to the team's Firestore Database.

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/FeedbackScreen.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/FeedbackScreen.java
@@ -2,12 +2,22 @@ package ca.hermeslogistics.itservices.petasosexpress;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /*
  * Names: Illia M. Popov, William Margalik, Dylan Ashton, Ahmad Aljawish
@@ -16,48 +26,82 @@ import android.view.ViewGroup;
  */
 public class FeedbackScreen extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private EditText editTextName;
+    private EditText editTextPhone;
+    private EditText editTextEmail;
+    private EditText editTextComment;
+    private RatingBar ratingBar;
+    private Button buttonSubmit;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FirebaseFirestore db;
 
     public FeedbackScreen() {
-        // Required empty public constructor
-    }
-
-    public static FeedbackScreen newInstance(String param1, String param2) {
-        FeedbackScreen fragment = new FeedbackScreen();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.feedback_screen, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Determine the device's current orientation
-        int orientation = getResources().getConfiguration().orientation;
-        View view;
-        // Load the appropriate layout based on orientation
-        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            view = inflater.inflate(R.layout.feedback_screen, container, false);
-        } else {
-            view = inflater.inflate(R.layout.feedback_screen_landscape, container, false);
-        }
+        db = FirebaseFirestore.getInstance();
+
+        // Initialize UI components
+        editTextName = view.findViewById(R.id.editTextName);
+        editTextPhone = view.findViewById(R.id.editTextPhone);
+        editTextEmail = view.findViewById(R.id.editTextEmail);
+        editTextComment = view.findViewById(R.id.editTextComment);
+        ratingBar = view.findViewById(R.id.ratingBar);
+        buttonSubmit = view.findViewById(R.id.buttonSubmit);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitFeedback();
+            }
+        });
+
         return view;
+    }
+
+    private void submitFeedback() {
+        String name = editTextName.getText().toString().trim();
+        String phone = editTextPhone.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String comment = editTextComment.getText().toString().trim();
+        float rating = ratingBar.getRating();
+
+        // Validate inputs
+        if (name.isEmpty() || phone.isEmpty() || email.isEmpty()) {
+            Toast.makeText(getContext(), "Please fill in all fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //Creating a random document ID
+        String documentId = generateRandomDocumentId(15);
+
+        //Creating a new feedback record
+        Map<String, Object> feedback = new HashMap<>();
+        feedback.put("name", name);
+        feedback.put("phone", Long.parseLong(phone));
+        feedback.put("email", email);
+        feedback.put("comment", comment);
+        feedback.put("rating", (long) rating);
+
+        //Adding the feedback to the 'feedbackRecord' collection
+        db.collection("feedbackRecord").document(documentId)
+                .set(feedback)
+                .addOnSuccessListener(aVoid -> Toast.makeText(getContext(), "Feedback submitted successfully!", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(getContext(), "Failed to submit feedback", Toast.LENGTH_SHORT).show());
+    }
+
+    private String generateRandomDocumentId(int targetStringLength) {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 }
