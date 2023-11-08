09aee9c1205ec6ab4f10fc7e3e21023351a1b63b
wmargalik <william.margalik@gmail.com>
2023-11-08 12:48:15 -0500

    PetasosExpress 1.07 (Search fragment UI) Implemented code, where once the user clicks the search bar, it brings you to a seperate fragment_search_screen.xml

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/Home.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/Home.java
@@ -1,25 +1,22 @@
 package ca.hermeslogistics.itservices.petasosexpress;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-/*
- * Names: Illia M. Popov, William Margalik, Dylan Ashton, Ahmad Aljawish
- * Student ID: n01421791, n01479878, n01442206, n01375348
- * Section: B
- */
+import ca.hermeslogistics.itservices.petasosexpress.SharedViewModel;
+
 public class Home extends Fragment {
 
     private EditText searchEditText;
+    private SharedViewModel sharedViewModel;
 
     public Home() {
         // Required empty public constructor
@@ -28,30 +25,41 @@ public class Home extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view;
-
-        int orientation = getResources().getConfiguration().orientation;
-
-        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            // Load landscape layout
-            view = inflater.inflate(R.layout.home_landscape, container, false);
-        } else {
-            // Load portrait layout
-            view = inflater.inflate(R.layout.fragment_home, container, false);
-        }
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         // Initialize UI elements
         searchEditText = view.findViewById(R.id.searchEditText);
 
-        // Set click listener for the search button
+        // Get the SharedViewModel instance
+        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+
+        // Set up click listener for the search button
         searchEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String searchQuery = searchEditText.getText().toString();
-                // Perform search based on searchQuery
+                // Update the search query in the SharedViewModel
+                sharedViewModel.setSearchQuery(searchQuery);
+
+                // Create the SearchScreen fragment
+                SearchScreen searchScreenFragment = new SearchScreen();
+
+                // Replace the current fragment with the SearchScreen fragment
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, searchScreenFragment) // R.id.fragment_container is the container in your activity layout where you want to replace the fragments
+                        .addToBackStack(null) // Optional: Allows the user to navigate back to the previous fragment
+                        .commit();
+
+                // Perform search based on searchQuery (optional)
                 String message = getString(R.string.searching_for, searchQuery);
                 Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
             }
+        });
+
+        // Observe changes to the search query
+        sharedViewModel.getSearchQuery().observe(getViewLifecycleOwner(), searchQuery -> {
+            // Update the UI or perform any action based on the new search query value
+            searchEditText.setText(searchQuery);
         });
 
         return view;
--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SearchScreen.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SearchScreen.java
@@ -1,61 +1,20 @@
 package ca.hermeslogistics.itservices.petasosexpress;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-/*
- * Names: Illia M. Popov, William Margalik, Dylan Ashton, Ahmad Aljawish
- * Student ID: n01421791, n01479878, n01442206, n01375348
- * Section: B
- */
 
 public class SearchScreen extends Fragment {
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private String mParam1;
-    private String mParam2;
 
     public SearchScreen() {
         // Required empty public constructor
     }
 
-    public static SearchScreen newInstance(String param1, String param2) {
-        SearchScreen fragment = new SearchScreen();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Determine the device's current orientation
-        int orientation = getResources().getConfiguration().orientation;
-
-        // Inflate the appropriate layout based on orientation
-        View view;
-        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            view = inflater.inflate(R.layout.search_screen_landscape, container, false);
-        } else {
-            view = inflater.inflate(R.layout.fragment_search_screen, container, false);
-        }
-
-        return view;
+        // Inflate the appropriate layout
+        return inflater.inflate(R.layout.fragment_search_screen, container, false);
     }
 }
