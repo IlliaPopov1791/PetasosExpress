33f4253c385f9714773a6627034c3542268612ca
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-11-07 20:16:14 -0400

    PetasosExpress 1.04 (Distance Sensor): distance sensor stops' history list was implemented.

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SensorDistance.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SensorDistance.java
@@ -10,15 +10,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
-import androidx.fragment.app.Fragment;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import javax.annotation.Nullable;
@@ -28,54 +36,52 @@ public class SensorDistance extends Fragment {
     private ImageView statusImg;
     private Double lastDistance = 0.0;
     private Double rpmValue = null;
-
-    // Firestore database reference
     private FirebaseFirestore db;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dateList = new ArrayList<>();
 
     public SensorDistance() {
+        // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Initialize Firestore
         db = FirebaseFirestore.getInstance();
-
-        // Determine the device's current orientation
         int orientation = getResources().getConfiguration().orientation;
-
         View view;
-        // Load the appropriate layout based on orientation
+
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
             view = inflater.inflate(R.layout.sensor_distance, container, false);
         } else {
             view = inflater.inflate(R.layout.sensor_distance_landscape, container, false);
         }
 
-        // Get a reference to UI objects
         txtValue = view.findViewById(R.id.txtValue);
         statusImg = view.findViewById(R.id.imgStatus);
-        //Ref for max speed
+        listView = view.findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, dateList);
+        listView.setAdapter(adapter);
+
         DocumentReference rpmRef = db.collection("Motors").document("PMfVBrNcmgOhjYlQZ4Ih");
         rpmRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot snapshot,
                                 @Nullable FirebaseFirestoreException e) {
                 if (e != null) {
-                    // Handle error
                     return;
                 }
                 if (snapshot != null && snapshot.exists()) {
                     Double rpm = snapshot.getDouble("rpm");
                     if (rpm != null) {
-                        rpmValue = rpm; // Store the RPM for use in distance update logic
+                        rpmValue = rpm;
                     }
                 }
             }
         });
-        // Get the document reference from Firestore
-        DocumentReference docRef = db.collection("Distance (Ultrasonic Sensor)").document("HChcHZOZwRMcrFVLnar4");
 
-        // Listen for realtime updates
+        DocumentReference docRef = db.collection("Distance (Ultrasonic Sensor)").document("HChcHZOZwRMcrFVLnar4");
         docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot snapshot,
@@ -86,24 +92,18 @@ public class SensorDistance extends Fragment {
                 }
 
                 if (snapshot != null && snapshot.exists()) {
-                    // Retrieve the 'Pulse Duration' and 'Speed of Sound' fields
                     Double pulseDuration = snapshot.getDouble("Pulse Duration");
                     Double speedOfSound = snapshot.getDouble("Speed of Sound ");
 
-                    // Check if both fields are retrieved successfully
                     if (pulseDuration != null && speedOfSound != null) {
-                        // Calculate the distance
                         Double distance = pulseDuration * speedOfSound;
-
                         txtValue.setText(String.format(Locale.getDefault(), "%.2f", distance));
                         updateStatus(statusImg, distance);
                         lastDistance = distance;
                     } else {
-                        //If the fields are not found or null
                         txtValue.setText(R.string.no_data);
                     }
                 } else {
-                    //If document does not exist
                     txtValue.setText(R.string.no_data);
                 }
             }
@@ -111,11 +111,13 @@ public class SensorDistance extends Fragment {
 
         return view;
     }
+
     private void updateStatus(ImageView imageView, double distance) {
         boolean isAccelerating = lastDistance != null && distance > lastDistance;
 
         if (distance < 0.1) {
             imageView.setImageResource(R.mipmap.speed_stop_round);
+            addStopTimeToList();
         } else if (distance >= 0.1 && distance <= 1.5) {
             if (rpmValue != null && rpmValue == 100) {
                 imageView.setImageResource(R.mipmap.speed_max_round);
@@ -130,5 +132,13 @@ public class SensorDistance extends Fragment {
             }
         }
     }
+
+    private void addStopTimeToList() {
+        String currentDateTime = DateFormat.getDateTimeInstance().format(new Date());
+        dateList.add(currentDateTime);
+        adapter.notifyDataSetChanged();
+        listView.smoothScrollToPosition(dateList.size() - 1);
+    }
 }
+
 
