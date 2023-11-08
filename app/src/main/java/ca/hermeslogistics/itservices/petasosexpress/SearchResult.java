112e56233ebac57eabe120a7320586b1485e8358
wmargalik <william.margalik@gmail.com>
2023-11-08 14:01:27 -0500

    PetasosExpress 1.08 (Search fragment UI) Implemented a code when once the database is setup for it, it will display search results onto the screen from the searchbar requests.

---
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SearchResult.java
@@ -0,0 +1,37 @@
+package ca.hermeslogistics.itservices.petasosexpress;
+
+public class SearchResult {
+    private String name;
+    private String address;
+    private String type;
+
+    public SearchResult(String name, String address, String type) {
+        this.name = name;
+        this.address = address;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
