aeac3480e707127e5695be0d7e6d4081cee00d05
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-11-10 18:48:07 -0400

    PetasosExpress 1.23 (Search Update): Now the Search uses data from our database.

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SearchScreen.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/SearchScreen.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,11 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -23,10 +27,11 @@ public class SearchScreen extends Fragment {
     private ListView searchStuffListView;
     private ArrayAdapter<String> adapter;
     private List<String> originalItems;
+    private List<String> fullItemList;
     private EditText searchEditText;
+    private FirebaseFirestore db;
 
     public SearchScreen() {
-        // Required empty public constructor
     }
 
     @Override
@@ -35,18 +40,13 @@ public class SearchScreen extends Fragment {
 
         searchStuffListView = view.findViewById(R.id.search_stuff);
         searchEditText = view.findViewById(R.id.searchEditText);
+        db = FirebaseFirestore.getInstance();
 
-        originalItems = Arrays.asList(getResources().getStringArray(R.array.my_stuff));
-
-        adapter = new ArrayAdapter<>(
-                requireContext(),
-                android.R.layout.simple_list_item_1,
-                new ArrayList<>(originalItems)
-        );
-
+        originalItems = new ArrayList<>();
+        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, originalItems);
         searchStuffListView.setAdapter(adapter);
+        fetchItems();
 
-        // Add a TextWatcher to listen for changes in the EditText
         searchEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
@@ -54,12 +54,7 @@ public class SearchScreen extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
-                // Get the category from the arguments
-                Bundle args = getArguments();
-                String category = (args != null && args.containsKey("category")) ? args.getString("category") : null;
-
-                // Filter the adapter based on the query text and category
-                filterAdapterAndCategory(charSequence.toString(), category);
+                filterAdapter(charSequence.toString());
             }
 
             @Override
@@ -67,92 +62,57 @@ public class SearchScreen extends Fragment {
             }
         });
 
-        // Set an OnEditorActionListener for the searchEditText
         searchEditText.setOnEditorActionListener((v, actionId, event) -> {
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                // Handle the search action here
                 String query = searchEditText.getText().toString().trim();
-                filterAdapterAndCategory(query, null);
-                // Close the keyboard
-                InputMethodManager imm = (InputMethodManager) requireContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(searchEditText.getWindowToken(), 0);
+                filterAdapter(query);
+                closeKeyboard();
                 return true;
             }
             return false;
         });
 
-        // Check if category is provided in arguments
-        Bundle args = getArguments();
-        if (args != null && args.containsKey("category")) {
-            String category = args.getString("category");
-            // Perform initial search based on the category
-            filterAdapterAndCategory("", category);
-        }
-
         return view;
     }
 
-    private void filterAdapterAndCategory(String query, String category) {
-        query = query.toLowerCase(Locale.getDefault());
-        adapter.clear();
+    private void fetchItems() {
+        db.collection("goods").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                fullItemList = new ArrayList<>();
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    String name = document.getString("name");
+                    Double price = document.getDouble("price");
 
-        if (query.length() == 0) {
-            // No query, show all items in the selected category
-            for (String item : originalItems) {
-                if (isItemInCategory(item, category)) {
-                    adapter.add(item);
+                    if (name != null && price != null) {
+                        String formattedItem = name + " - $" + String.format(Locale.getDefault(), "%.2f", price);
+                        fullItemList.add(formattedItem);
+                        originalItems.add(formattedItem);
+                    }
                 }
+                adapter.notifyDataSetChanged();
+            } else {
+                Log.e("FetchItems", "Error fetching documents", task.getException());
             }
-        } else {
-            // Filter based on both query and category
-            for (String item : originalItems) {
-                if (item.toLowerCase(Locale.getDefault()).contains(query) && isItemInCategory(item, category)) {
-                    adapter.add(item);
-                }
+        });
+    }
+
+    private void filterAdapter(String query) {
+        query = query.toLowerCase(Locale.getDefault());
+        List<String> filteredList = new ArrayList<>();
+
+        for (String item : fullItemList) {
+            if (item.toLowerCase(Locale.getDefault()).contains(query)) {
+                filteredList.add(item);
             }
         }
 
+        adapter.clear();
+        adapter.addAll(filteredList);
         adapter.notifyDataSetChanged();
     }
 
-    private boolean isItemInCategory(String item, String category) {
-        if (category == null || category.isEmpty()) {
-            return true; // No specific category, always include the item
-        }
-
-        item = item.toLowerCase(Locale.getDefault()); // Convert item to lowercase for case-insensitive comparison
-
-        String[] foodItems = {"pizza hut", "kfc", "mcdonald's", "starbucks"};
-        String[] electronicsItems = {"best buy", "staples"};
-        String[] medicalItems = {"shoppers drug mart", "circle k"};
-        String[] otherItems = {"walmart", "bed bath & beyond", "fortinos", "humber bookstore", "costco"};
-
-        String[] selectedItems;
-
-        switch (category.toLowerCase(Locale.getDefault())) {
-            case "food":
-                selectedItems = foodItems;
-                break;
-            case "electronics":
-                selectedItems = electronicsItems;
-                break;
-            case "medical":
-                selectedItems = medicalItems;
-                break;
-            case "other":
-                selectedItems = otherItems;
-                break;
-            default:
-                return false; // Unknown category
-        }
-
-        // Check if the item belongs to the selected category
-        for (String selectedItem : selectedItems) {
-            if (item.contains(selectedItem)) {
-                return true;
-            }
-        }
-
-        return false;
+    private void closeKeyboard() {
+        InputMethodManager imm = (InputMethodManager) requireContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(searchEditText.getWindowToken(), 0);
     }
 }
