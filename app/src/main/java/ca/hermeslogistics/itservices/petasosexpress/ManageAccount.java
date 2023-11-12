6ae428c978cf316350d7409f48e3760ec5ca2c2f
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-11-11 22:14:15 -0400

    PetasosExpress 1.39 (Manage Account Commit):Manage account now is semi-functional the user can now freely change their phone, first and last name. Email and Profile Picture are unchangeable for now.

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/ManageAccount.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/ManageAccount.java
@@ -1,12 +1,20 @@
 package ca.hermeslogistics.itservices.petasosexpress;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 /*
  * Names: Illia M. Popov, William Margalik, Dylan Ashton, Ahmad Aljawish
@@ -15,11 +23,80 @@ import android.view.ViewGroup;
  */
 public class ManageAccount extends Fragment {
 
+    private FirebaseFirestore db;
+    private FirebaseAuth auth;
+    private Button btnSave;
+    private EditText inputEmail, inputFirstName, inputLastName, inputPhone;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.account_manage, container, false);
+        View view = inflater.inflate(R.layout.account_manage, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        inputEmail = view.findViewById(R.id.inputEmail);
+        inputFirstName = view.findViewById(R.id.inputFirstName);
+        inputLastName = view.findViewById(R.id.inputLastName);
+        inputPhone = view.findViewById(R.id.inputPhone);
+
+        loadUserData();
+
+        btnSave = view.findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserData();
+            }
+        });
+
+        return view;
+    }
+
+    private void saveUserData() {
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            String userEmail = currentUser.getEmail();
+            if (userEmail != null) {
+                String newFirstName = inputFirstName.getText().toString();
+                String newLastName = inputLastName.getText().toString();
+                String newPhone = inputPhone.getText().toString();
+
+                db.collection("userInfo").document(userEmail)
+                        .update("firstName", newFirstName,
+                                "lastName", newLastName,
+                                "phone", Long.parseLong(newPhone))
+                        .addOnSuccessListener(aVoid -> {
+                            Toast.makeText(getContext(), "Data updated successfully", Toast.LENGTH_SHORT).show();
+                        })
+                        .addOnFailureListener(e -> {
+                            Toast.makeText(getContext(), "Error updating data", Toast.LENGTH_SHORT).show();
+                        });
+            }
+        }
+    }
+
+    private void loadUserData() {
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            String userEmail = currentUser.getEmail();
+            if (userEmail != null) {
+                inputEmail.setText(userEmail); // Set email in the email field
+                db.collection("userInfo").document(userEmail).get().addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        String firstName = documentSnapshot.getString("firstName");
+                        String lastName = documentSnapshot.getString("lastName");
+                        Long phone = documentSnapshot.getLong("phone");
+
+                        inputFirstName.setText(firstName != null ? firstName : "");
+                        inputLastName.setText(lastName != null ? lastName : "");
+                        inputPhone.setText(phone != null ? String.valueOf(phone) : "");
+                    }
+                }).addOnFailureListener(e -> {
+
+                });
+            }
+        }
     }
 }
