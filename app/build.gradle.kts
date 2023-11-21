d74293458b4a77858c2fa78d04c9ab5c41c0e972
ahmadaljawish5348 <Ahmadjaweesh6@gmail.com>
2023-11-21 18:47:47 -0500

    PetasosExpress 1.59 (gradebuild Commit): adding dependencies for junit testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,18 +33,22 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-analytics")
-    implementation ("com.google.firebase:firebase-database:20.0.1")
+    implementation("com.google.firebase:firebase-database:20.0.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.google.firebase:firebase-auth:22.1.2")
-    implementation ("com.google.firebase:firebase-firestore:24.0.0")
+    implementation("com.google.firebase:firebase-firestore:24.0.0")
+
+    // Unit Test Dependencies
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:3.3.3")
+
+    // Android Test Dependencies
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
