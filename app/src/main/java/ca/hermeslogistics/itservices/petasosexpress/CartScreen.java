0f60bd38093c559d011881156a529b9cb0c95630
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-11-25 17:53:46 -0400

    PetasosExpress 1.74 (Cart Commit): Cart Screen now shows products in the cart user did not pay yet. Also, calculates total price of the cart content and displays it for the user.

--- app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/CartScreen.java
+++ app/src/main/java/ca/hermeslogistics/itservices/petasosexpress/CartScreen.java
@@ -3,62 +3,84 @@ package ca.hermeslogistics.itservices.petasosexpress;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CartScreen#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class CartScreen extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CartScreen() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CartScreen.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CartScreen newInstance(String param1, String param2) {
-        CartScreen fragment = new CartScreen();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ListView cartItemsListView;
+    private TextView totalAmountTextView;
+    private Button checkoutButton;
+    private ProductAdapter productAdapter;
+    private List<Product> productList;
+    private FirebaseFirestore db;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.cart_screen, container, false);
+
+        cartItemsListView = view.findViewById(R.id.cart_items_list);
+        totalAmountTextView = view.findViewById(R.id.total_amount);
+        checkoutButton = view.findViewById(R.id.checkout_button);
+        db = FirebaseFirestore.getInstance();
+
+        productList = new ArrayList<>();
+        productAdapter = new ProductAdapter(requireContext(), productList);
+        cartItemsListView.setAdapter(productAdapter);
+
+        loadCartItems();
+
+        return view;
+    }
+    private void loadCartItems() {
+        SharedPreferences sharedPrefs = getActivity().getSharedPreferences("CartPrefs", Context.MODE_PRIVATE);
+        String cartJson = sharedPrefs.getString("cart", "[]");
+        AtomicReference<Double> totalAmount = new AtomicReference<>(0.0);
+
+        try {
+            JSONArray cartArray = new JSONArray(cartJson);
+            for (int i = 0; i < cartArray.length(); i++) {
+                JSONObject cartItem = cartArray.getJSONObject(i);
+                int productId = cartItem.getInt("id");
+                int quantity = cartItem.getInt("quantity");
+
+                String documentId = String.format("%012d", productId);
+
+                db.collection("goods").document(documentId).get().addOnSuccessListener(documentSnapshot -> {
+                    String name = documentSnapshot.getString("name");
+                    Double price = documentSnapshot.getDouble("price");
+                    String producer = documentSnapshot.getString("producer");
+                    String type = documentSnapshot.getString("type");
+
+                    if (name != null && price != null) {
+                        Product product = new Product(name, productId, price, producer, type);
+                        productList.add(product);
+                        productAdapter.notifyDataSetChanged();
+
+                        //Calculating total amount
+                        totalAmount.updateAndGet(v -> Double.valueOf(v + price * quantity));
+                        totalAmountTextView.setText(String.format(Locale.getDefault(), "$%.2f", totalAmount.get()));
+                    }
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.cart_screen, container, false);
     }
 }
